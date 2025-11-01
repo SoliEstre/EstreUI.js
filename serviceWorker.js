1c167bd489046f7fd796dbaf6bd4eb4c5c914c02
SoliEstre <estresoliette@gmail.com>
2025-11-01 15:04:50 +0900

    Applied support related path for cache file list

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,69 +1,70 @@
-const INSTALLATION_VERSION_NAME = "0.9.0-r20251101a";
+const INSTALLATION_VERSION_NAME = "0.9.0-r20251101b";
 // ^^ Use for check new update "Native application(webview) version(or Android/iOS version combo) - PWA release version"
 // ex) "1.0.1/1.0.0-r20251101k"
  
 const INSTALLATION_FILE_LIST = [
-    "/serviceWorker.js",
-    "/fixedTop.html",
-    "/fixedBottom.html",
-    "/mainMenu.html",
-    "/staticDoc.html",
-    "/instantDoc.html",
-    "/managedOverlay.html",
+    "./serviceWorker.js",
+    "./fixedTop.html",
+    "./fixedBottom.html",
+    "./mainMenu.html",
+    "./staticDoc.html",
+    "./instantDoc.html",
+    "./managedOverlay.html",
 
 
-    "/styles/main.css",
+    "./styles/main.css",
 
 
-    "/scripts/main.js",
+    "./scripts/main.js",
 ];
 
 
 // Common files cache - Be changes some time but, well not changed very often
-const CACHE_NAME_COMMON_FILES = "common-files-cache-v1-20251101a";
+const CACHE_NAME_COMMON_FILES = "common-files-cache-v1-20251101b";
 
 const COMMON_FILES_TO_CACHE = [
-    "/",
-    "/index.html",
-    "/serviceLoader.html",
-    "/webmanifest.json",
+    "./",
+    "./index.html",
+    "./serviceLoader.html",
+    "./webmanifest.json",
 
 
-    "/styles/estreUiInitialize.css",
-    "/styles/estreUiRoot.css",
-    "/styles/estreUiCore.css",
-    "/styles/estreUiCore2.css",
-    "/styles/estreUi.css",
-    "/styles/estreUiHandles.css",
-    "/styles/estreUiHandleUnical.css",
+    "./styles/estreUiInitialize.css",
+    "./styles/estreUiRoot.css",
+    "./styles/estreUiCore.css",
+    "./styles/estreUiCore2.css",
+    "./styles/estreUiAliases.css",
+    "./styles/estreUi.css",
+    "./styles/estreUiHandles.css",
+    "./styles/estreUiHandleUnical.css",
 
 
-    "/scripts/jcodd.js",
-    "/scripts/modernism.js",
-    "/scripts/alienese.js",
-    "/scripts/estreU0EEOZ.js",
-    "/scripts/estreUi.js",
+    "./scripts/jcodd.js",
+    "./scripts/modernism.js",
+    "./scripts/alienese.js",
+    "./scripts/estreU0EEOZ.js",
+    "./scripts/estreUi.js",
 ];
 
 
 // Static files cache - Rarely changes after release
-const CACHE_NAME_STATIC_FILES = "static-files-cache-v1-20251101";
+const CACHE_NAME_STATIC_FILES = "static-files-cache-v1-20251101b";
 
 const STATIC_FILES_TO_CACHE = [
-    "/favicon.ico",
+    "./favicon.ico",
 
 
     "https://raw.githack.com/googlefonts/noto-emoji/main/fonts/NotoColorEmoji.ttf",
 
 
-    "/images/app_icon_16x16.png",
-    "/images/app_icon_32x32.png",
-    "/images/app_icon_48x48.png",
-    "/images/app_icon_144x144.png",
-    "/images/app_icon_167x167.png",
-    "/images/app_icon_180x180.png",
-    "/images/app_icon_192x192.png",
-    "/images/app_icon_512x512.png",
+    "./images/app_icon_16x16.png",
+    "./images/app_icon_32x32.png",
+    "./images/app_icon_48x48.png",
+    "./images/app_icon_144x144.png",
+    "./images/app_icon_167x167.png",
+    "./images/app_icon_180x180.png",
+    "./images/app_icon_192x192.png",
+    "./images/app_icon_512x512.png",
 
 
     "https://fonts.googleapis.com/css2?family=Cute+Font&family=Noto+Sans+KR:wght@100..900&display=swap",
@@ -73,26 +74,26 @@ const STATIC_FILES_TO_CACHE = [
     "https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs",
 
 
-    "/lotties/menu_and_back_btn.json",
-    "/lotties/menu_and_close_btn.json",
-    "/lotties/loading_circle.json",
-    "/lotties/ptr_indic.json",
-    "/lotties/progress_bar.json",
+    "./lotties/menu_and_back_btn.json",
+    "./lotties/menu_and_close_btn.json",
+    "./lotties/loading_circle.json",
+    "./lotties/ptr_indic.json",
+    "./lotties/progress_bar.json",
 
-    "/vectors/more_vertical_slim_icon.svg",
-    "/vectors/app_icon.svg",
+    "./vectors/more_vertical_slim_icon.svg",
+    "./vectors/app_icon.svg",
 ];
 
 
 // Stony files cache - Very frequently changes and heavy sized files (for background caching)
-const CACHE_NAME_STONY_FILES = "stony-files-cache-v1-20251101";
+const CACHE_NAME_STONY_FILES = "stony-files-cache-v1-20251101b";
 
 const STONY_FILES_TO_CACHE = [
 ];
 
 
 const CHECK_ALWAYS_NEWER_FILE_LIST = [
-    "/serviceWorker.js",
+    "./serviceWorker.js",
 ];
 
 
@@ -120,6 +121,7 @@ Object.defineProperty(self, "isVerbosely", {
 
 self.addEventListener("install", (event) => {
     if (self.isLogging) console.log("Service Worker - Install service worker with cache list" + INSTALLATION_VERSION_NAME);
+    const scope = self.registration.scope;
     // vv When use for force installing the new service worker always (not recommended)
     //self.skipWaiting();
     event.waitUntil(
@@ -127,22 +129,22 @@ self.addEventListener("install", (event) => {
             const loaders = [];
             if (!keyList.includes(CACHE_NAME_COMMON_FILES)) {
                 if (self.isLogging) console.log("Service Worker - Caching common files - " + CACHE_NAME_COMMON_FILES);
-                loaders.push(caches.open(CACHE_NAME_COMMON_FILES).then((cache) => cache.addAll(COMMON_FILES_TO_CACHE)));
+                loaders.push(caches.open(CACHE_NAME_COMMON_FILES).then((cache) => cache.addAll(COMMON_FILES_TO_CACHE.map(it => it.replace(/^\.\//, scope)))));
             }
             if (!keyList.includes(CACHE_NAME_STATIC_FILES)) {
                 if (self.isLogging) console.log("Service Worker - Caching static files - " + CACHE_NAME_STATIC_FILES);
-                loaders.push(caches.open(CACHE_NAME_STATIC_FILES).then((cache) => cache.addAll(STATIC_FILES_TO_CACHE)));
+                loaders.push(caches.open(CACHE_NAME_STATIC_FILES).then((cache) => cache.addAll(STATIC_FILES_TO_CACHE.map(it => it.replace(/^\.\//, scope)))));
             }
             if (!keyList.includes(INSTALLATION_VERSION_NAME)) {
                 if (self.isLogging) console.log("Service Worker - Caching application files - " + INSTALLATION_VERSION_NAME);
-                loaders.push(caches.open(INSTALLATION_VERSION_NAME).then((cache) => cache.addAll(INSTALLATION_FILE_LIST)));
+                loaders.push(caches.open(INSTALLATION_VERSION_NAME).then((cache) => cache.addAll(INSTALLATION_FILE_LIST.map(it => it.replace(/^\.\//, scope)))));
             }
             await Promise.all(loaders);
             if (self.isLogging) console.log("Service Worker - All required files are cached");
 
             if (!keyList.includes(CACHE_NAME_STATIC_FILES)) (async _ => {
                 if (self.isLogging) console.log("Service Worker - Caching static files - " + CACHE_NAME_STATIC_FILES);
-                await caches.open(CACHE_NAME_STATIC_FILES).then((cache) => cache.addAll(STATIC_FILES_TO_CACHE));
+                await caches.open(CACHE_NAME_STATIC_FILES).then((cache) => cache.addAll(STATIC_FILES_TO_CACHE.map(it => it.replace(/^\.\//, scope))));
                 if (self.isLogging) console.log("Service Worker - Static files are cached");
             })();
 
@@ -170,6 +172,7 @@ self.addEventListener("activate", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
+    const scope = self.registration.scope;
     const urlString = event.request.url;
     const url = new URL(urlString);
     const pathname = url.pathname;
@@ -180,8 +183,8 @@ self.addEventListener("fetch", (event) => {
                 return (await caches.match(event.request)) ?? error;
             })
         );
-    } else if (url.pathname.includes("/|") || url.pathname.includes("/%7C")) event.respondWith(EMPTY_RESPONSE);
-    else if (COMMON_FILES_TO_CACHE.find(it => urlString.includes(it)) || INSTALLATION_FILE_LIST.find(it => urlString.includes(it)) || STATIC_FILES_TO_CACHE.find(it => urlString.includes(it)) || STONY_FILES_TO_CACHE.find(it => urlString.includes(it))) {
+    } else if (pathname.includes("/|") || pathname.includes("/%7C")) event.respondWith(EMPTY_RESPONSE);
+    else if (COMMON_FILES_TO_CACHE.find(it => urlString == it.replace(/^\.\//, scope)) || INSTALLATION_FILE_LIST.find(it => urlString == it.replace(/^\.\//, scope)) || STATIC_FILES_TO_CACHE.find(it => urlString == it.replace(/^\.\//, scope)) || STONY_FILES_TO_CACHE.find(it => urlString == it.replace(/^\.\//, scope))) {
         event.respondWith((async () => {
             // if (self.isLogging) console.log("Service Worker - Fetch intercepted for: ", urlString);
             // Try to get the response from a cache.
