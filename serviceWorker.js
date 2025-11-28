ee42fd8d1fceb879c44b2595d6ec5ce2bae55867
SoliEstre <estresoliette@gmail.com>
2025-11-28 14:20:36 +0900

    chore: Bump package version and service worker installation version to 1.0.0.RC4.

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,4 @@
-const INSTALLATION_VERSION_NAME = "1.0.0.RC3-r20251126m";
+const INSTALLATION_VERSION_NAME = "1.0.0.RC4-r20251128";
 // ^^ Use for check new update "Native application(webview) version(or Android/iOS version combo) - PWA release version"
 // ex) "1.0.1/1.0.0-r20251101k"
 
