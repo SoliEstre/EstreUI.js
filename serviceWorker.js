67f1076f14bb5f2dd77386914f9e247fdfe885ae
SoliEstre <estresoliette@gmail.com>
2025-12-09 18:38:23 +0900

    Something updated and fixed

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,4 @@
-const INSTALLATION_VERSION_NAME = "1.0.0-r20251128";
+const INSTALLATION_VERSION_NAME = "1.0.1-r20251128";
 // ^^ Use for check new update "Native application(webview) version(or Android/iOS version combo) - PWA release version"
 // ex) "1.0.1/1.0.0-r20251101k"
 
@@ -21,7 +21,7 @@ const INSTALLATION_FILE_LIST = [
 
 
 // Common files cache - Be changes some time but, well not changed very often
-const CACHE_NAME_COMMON_FILES = "common-files-cache-v1-20251126";
+const CACHE_NAME_COMMON_FILES = "common-files-cache-v1-20251209";
 
 const COMMON_FILES_TO_CACHE = [
     "./",
